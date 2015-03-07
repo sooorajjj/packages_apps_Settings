167757a63c69130592fb2b606129d8ee20ee9118
Michael Bestas <mikeioannina@gmail.com>
2015-03-07 05:22:23 +0200

    Allow launching display rotation & lock screen settings externally

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -107,6 +107,7 @@ public class Settings extends SettingsActivity {
     public static class SoundSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LockScreenSettingsActivity extends SettingsActivity { /* empty */ }
     public static class LiveDisplayActivity extends SettingsActivity { /* empty */ }
+    public static class DisplayRotationActivity extends SettingsActivity { /* empty */ }
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
     public static class AmbientDisplaySettingsActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.cyanogenmod.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -323,6 +324,7 @@ public class SettingsActivity extends Activity
 			com.android.settings.sokp.SOKPControl.class.getName(),
             LockScreenSettings.class.getName(),
             LiveDisplay.class.getName(),
+			DisplayRotation.class.getName(),
             ProfilesSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             AmbientSettings.class.getName()
